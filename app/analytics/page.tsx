3c14a7f457a04f402ae9c4420cefd2f30dd4c079
hernandes-code <hernandesalmeida5@gmail.com>
2026-09-15 03:46:03 -0300

    Add analytics route

---
+++ app/analytics/page.tsx
@@ -0,0 +1,3 @@
+import { AppShell } from '@/components/core-ui';
+import { AnalyticsView } from '@/components/analytics-view';
+export default function AnalyticsPage(){return <AppShell><AnalyticsView/></AppShell>}
