8bc80fdd4739943bc8eb1bc6300691ede57dac5e
hernandes-code <hernandesalmeida5@gmail.com>
2026-09-15 03:46:11 -0300

    Add profile route

---
+++ app/profile/page.tsx
@@ -0,0 +1,3 @@
+import { AppShell } from '@/components/core-ui';
+import { ProfileView } from '@/components/profile-view';
+export default function ProfilePage(){return <AppShell><ProfileView/></AppShell>}
