61626fb508b48a7092b2d60cf639f443d2d8300a
hernandes-code <hernandesalmeida5@gmail.com>
2026-09-15 03:45:56 -0300

    Add sessions route

---
+++ app/sessions/page.tsx
@@ -0,0 +1,3 @@
+import { AppShell } from '@/components/core-ui';
+import { SessionsView } from '@/components/sessions-view';
+export default function SessionsPage(){return <AppShell><SessionsView/></AppShell>}
