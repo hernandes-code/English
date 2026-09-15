effb32e3b1bccf1de14889c884d4647e53333ceb
hernandes-code <hernandesalmeida5@gmail.com>
2026-09-15 03:45:48 -0300

    Add skills route

---
+++ app/skills/page.tsx
@@ -0,0 +1,3 @@
+import { AppShell } from '@/components/core-ui';
+import { SkillsView } from '@/components/skills-view';
+export default function SkillsPage(){return <AppShell><SkillsView/></AppShell>}
