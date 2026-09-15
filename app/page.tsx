16a53352def428729e40196301c69e0ca66e8d75
hernandes-code <hernandesalmeida5@gmail.com>
2026-09-15 03:45:41 -0300

    Add dashboard route

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+import { AppShell } from '@/components/core-ui';
+import { DashboardView } from '@/components/dashboard-view';
+export default function HomePage(){return <AppShell><DashboardView/></AppShell>}
