d8f53d418ae6edc50ce9ceedcb496a8cf118735d
hernandes-code <hernandesalmeida5@gmail.com>
2026-09-17 14:13:51 -0300

    Move Sessions under persistent Paper Book layout

--- app/sessions/page.tsx
+++ app/sessions/page.tsx
@@ -1,3 +1,2 @@
-import { PaperBookShell } from '@/components/paper-book-shell';
 import { SessionsView } from '@/components/sessions-view';
-export default function SessionsPage(){return <PaperBookShell><SessionsView/></PaperBookShell>}
+export default function SessionsPage(){return <SessionsView/>}
