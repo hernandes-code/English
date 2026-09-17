de3c2804ae65e08a6e80bf972beefc934bcd0202
hernandes-code <hernandesalmeida5@gmail.com>
2026-09-17 14:13:42 -0300

    Move Skills under persistent Paper Book layout

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,3 +1,2 @@
-import { PaperBookShell } from '@/components/paper-book-shell';
 import { SkillsView } from '@/components/skills-view';
-export default function SkillsPage(){return <PaperBookShell><SkillsView/></PaperBookShell>}
+export default function SkillsPage(){return <SkillsView/>}
